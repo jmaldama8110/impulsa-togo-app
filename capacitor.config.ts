1adca1df84670d7bf295c1fa7327620600e7f6b7
Josman <josemanuek@hotmail.com>
2022-12-01 09:46:53 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'impulsa-togo-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
